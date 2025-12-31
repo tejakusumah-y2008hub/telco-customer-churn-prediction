82fa97f5abc8a6eaa4900b1c25f132984a3c7a7c
Yusuf Tejakusumah <tejakusumahyusuf832@gmail.com>
2026-01-01 00:17:10 +0700

    Modify test_data and add a basic Smoke Test that checks if processed data exists

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,2 +1,17 @@
-def test_code_is_tested():
-    pass
+import pytest
+
+from telco_customer_churn_prediction.config import PROCESSED_DATA_DIR
+
+
+def test_processed_data_exists():
+    """
+    Simple smoke test to ensure the processed train file exists.
+    """
+    train_file = PROCESSED_DATA_DIR / "train_df.parquet"
+
+    # If the file doesn't exist, we skip the test instead of failing,
+    # because maybe the user hasn't run 'make data' yet.
+    if not train_file.exists():
+        pytest.skip("Processed data not found. Run 'make features' first.")
+
+    assert train_file.exists()
